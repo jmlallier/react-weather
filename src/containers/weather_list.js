641a18c7f78641f2eb1a1a2139ded881d99f87c6
John-Michael L'Allier <jmlallier@me.com>
2017-07-19 22:32:50 -0400

    initial commit

---
+++ src/containers/weather_list.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
+
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name       = cityData.city.name;
+		const id         = cityData.city.id;
+		const temps      = _.map(cityData.list.map(weather => weather.main.temp), num => {
+			return ((9 / 5 * (num - 273)) + 32);
+		});
+		const pressures  = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const {lon, lat} = cityData.city.coord;
+		
+		return (
+			<tr key={id}>
+				<td>
+					<GoogleMap lat={lat} lng={lon}/>
+				</td>
+				<td><Chart data={temps} color="red" units="&deg;F"/></td>
+				<td><Chart data={pressures} color="green" units="hPa"/></td>
+				<td><Chart data={humidities} color="blue" units="%"/></td>
+			</tr>
+		);
+	}
+	
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+				<tr>
+					<th>City</th>
+					<th>Temperature (F)</th>
+					<th>Pressure (hPa)</th>
+					<th>Humidity (%)</th>
+				</tr>
+				</thead>
+				<tbody>
+				{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+function mapStateToProps({weather}) {
+	return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
